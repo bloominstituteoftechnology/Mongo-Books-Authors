805d1318064509f2251e621406545be861157517
Ryan Hamblin <ryanhca@users.noreply.github.com>
2017-10-18 15:42:50 -0600

    Had wrong instructions

--- models.js
+++ models.js
@@ -23,38 +23,3 @@ const PersonSchema = ({
 })
 
 module.exports = mongoose.model('Person', PersonSchema);
-
-/* pull in mongoose and write yoru schema here.
-*
-* we are building for this type of collection
-* {
-  "firstName": "Jedd",
-  "lastName": "Challener",
-  "email": "jchallener3@blinklist.com",
-  "friends": [
-      {
-          "gender": "Female",
-          "firstName": "Giustina",
-          "lastName": "Scurr",
-          "age": 84
-      },
-      {
-          "gender": "Female",
-          "firstName": "Vevay",
-          "lastName": "Johnke",
-          "age": 21
-      },
-      {
-          "gender": "Female",
-          "firstName": "Mildrid",
-          "lastName": "Jackes",
-          "age": 24
-      }
-  ],
-  "age": 73,
-  "gender": "Male",
-  "location": "Tennessee",
-  "dateOfBirth": "6/10/2017"
-},
-*}
-*/
