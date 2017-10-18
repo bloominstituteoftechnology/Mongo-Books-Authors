504d9dde0511a5ecbc894b88612982c91e081366
Ryan Hamblin <ryanhca@gmail.com>
2017-10-18 15:56:37 -0600

    fooin around with the server vs app.

--- server.js
+++ server.js
@@ -5,28 +5,29 @@ const Person = require('./models.js');
 
 const port = process.env.PORT || 3000;
 
-const app = express();
+const server = express();
 
 // error status code constants
 const STATUS_SERVER_ERROR = 500;
 const STATUS_USER_ERROR = 422;
 
-app.use(bodyParser.json());
+server.use(bodyParser.json());
 
 // Your API will be built out here.
 
-
 mongoose.Promise = global.Promise;
-const connect = mongoose.connect(
-  'mongodb://localhost/people',
-  { useMongoClient: true }
-);
-/* eslint no-console: 0 */
-connect.then(() => {
-  app.listen(port);
-  console.log(`Server Listening on ${port}`);
-}, (err) => {
-  console.log('\n************************');
-  console.log("ERROR: Couldn't connect to MongoDB. Do you have it running?");
-  console.log('************************\n');
+const connect = mongoose.connect('mongodb://localhost/people', {
+  useMongoClient: true
 });
+/* eslint no-console: 0 */
+connect.then(
+  () => {
+    server.listen(port);
+    console.log(`Server Listening on ${port}`);
+  },
+  err => {
+    console.log('\n************************');
+    console.log("ERROR: Couldn't connect to MongoDB. Do you have it running?");
+    console.log('************************\n');
+  }
+);
