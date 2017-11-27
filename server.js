c45bcc39d88180036fa1a16b6f215ab0fffb08b6
alexfigliolia <alexfigliolia@gmail.com>
2017-11-27 14:53:55 -0500

    upload

--- server.js
+++ server.js
@@ -31,3 +31,60 @@ connect.then(
     console.log('************************\n');
   }
 );
+
+server.get('/users', (req, res) => {
+  Person.find((err, users) => {
+    if(err) {
+      res
+        .status(STATUS_SERVER_ERROR)
+        .json({'Error retrieving users:': err});
+    } else {
+      res.status(200).json(users);
+    }
+  });
+});
+
+server.get('/users/:direction', (req, res) => {
+  const { direction } = req.params; 
+  Person.find({})
+    .sort({'firstName': direction})
+    .exec((err, sortedUsers) => {
+      if(err) {
+        res
+          .status(STATUS_USER_ERROR)
+          .json({'Error retrieving users:': err});
+      } else {
+        res.status(200).json(sortedUsers);
+      }
+    });
+});
+
+server.get('/user-get-friends/:id', (req, res) => {
+  const { id } = req.params;
+  if(id !== undefined) {
+    Person.findById(id)
+      .select('friends')
+      .exec((err, friends) => {
+        if(err) {
+          res.status(STATUS_USER_ERROR).json({'could not find user with that id': err});
+        } else {
+          res.status(200).json(friends);
+        }
+      });
+  }
+});
+
+server.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName } = req.body;
+  if(id !== undefined) {
+    Person.findByIdAndUpdate(id, {firstName, lastName}, {new: true})
+      .exec((err, update) => {
+        if(err) {
+          res.status(STATUS_USER_ERROR).json({'could not update user with that id': err});
+        } else {
+          res.status(200).json(update);
+        }
+      });
+  }
+});
